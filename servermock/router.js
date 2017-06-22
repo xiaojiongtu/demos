ea27b03c159acfc3bf93685cf67051fa3156c45e
xiaojiongtu <18377335170@163.com>
2017-06-22 11:17:57 +0800

    加载更多

---
+++ servermock/router.js
@@ -0,0 +1,9 @@
+app.get('/loadMore',function(req,res){
+   var index=req.query.index;
+    var len=req.query.length;
+    var data=[];
+    for(var i=0;i<len;i++){
+        data.push('ÐÂÎÅ'+(parseInt(index)+i));
+    }
+      res.send(data);
+});
